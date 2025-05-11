f7cceab785b6a2bd0910a19cbf2e0974fcf2a61e
gwaDyckuL1 <gwaDyckuL@yahoo.com>
2025-05-10 18:29:19 -0700

    add dir creation on startup

--- cmd/lotto-scraper/main.go
+++ cmd/lotto-scraper/main.go
@@ -4,12 +4,22 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/gwaDyckuL1/lotto-scraper/ui"
 	_ "modernc.org/sqlite"
 )
 
 func main() {
+
+	requiredDirs := []string{"data", "graphs"}
+	for _, dir := range requiredDirs {
+		err := os.MkdirAll(dir, os.ModePerm)
+		if err != nil {
+			log.Fatalf("Failed to create folder %s: %v", dir, err)
+		}
+	}
+
 	fmt.Print("\033[H\033[2J")
 	db, err := sql.Open("sqlite", "data/lotto.db")
 	if err != nil {
